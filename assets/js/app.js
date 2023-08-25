39d43e595e6ba6ad4e3b038b86c5c88333ec0401
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-08-25 14:22:13 +0530

    change in html or css and js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,5 @@
 let nav = document.querySelector(".nav");
 let menubar = document.querySelector(".menubar");
-
-console.log(nav)
-console.log(menubar)
-
 nav.addEventListener("click", function () {
-    menubar.classList.toggle("left-0")
-
+    menubar.classList.toggle("right-0")
 })
